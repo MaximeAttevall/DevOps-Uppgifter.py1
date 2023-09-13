0ca40bd38b849baf49db8a2138b24a50bb3e6cf9
maxime <max.attevall@gmail.com>
2023-09-13 19:08:26 +0200

    Uppgifter-Kompendium

--- lektion 5/uppgifter 11.3.py
+++ lektion 5/uppgifter 11.3.py
@@ -10,7 +10,9 @@ if os.path.exists(file_name):
 else:
     nummer = []
 
+
 summa = sum(nummer)
+
 
 while True:
 
@@ -23,22 +25,25 @@ while True:
     print(".: intMEMERIZER :.".center(ui_width))
     print("*" * ui_width)
     print("-" * ui_width)
+    for x in nummer:
+        print("*", x)
     print("summa: ", summa)
     print("-" * ui_width)
     print("- Mata in heltal")
     print("- 0 stänger scriptet")
     print("-" * ui_width)
 
-    rensa = input("önskar du rensa listan ja/nej: ").lower()
+    """rensa = input("önskar du rensa listan ja/nej: ").lower()
     if rensa == "ja":
         nummer.clear()
         with open(file_name, "w") as f:
             json.dump(nummer, f)
     elif rensa == "nej":
-        print("listan fortsätter...")
+        print("listan fortsätter, mata in heltal nedanför")
     else:
         print("skriv in ja eller nej och försök igen")
         continue
+        """
 
     try:
         value = int(input("> "))
@@ -50,6 +55,10 @@ while True:
     summa += value
 
     if value == 0:
+        print("listan rensas till nästa försök.")
+        nummer.clear()
+        with open(file_name, "w") as f:
+            json.dump(nummer, f)
         break
 
     with open(file_name, "w") as f:
